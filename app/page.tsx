2af3ef2241b92b719fe2fb748dca2235dd4ea1ff
Grdmrthh <dnkw@GMHs-Macbook-Pro.local>
2026-06-08 23:51:18 +0700

    Update homepage and articles

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ return (
     <div className="absolute top-5 left-8 right-8 z-[9999] flex justify-between items-start">
 
       <Image
-        src="/Logo_Delvora.jpeg"
+        src="/Logo_Delvora2.jpeg"
         alt="Logo"
         width={200}
         height={200}
